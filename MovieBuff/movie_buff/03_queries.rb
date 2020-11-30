1b24e662417b02793d3fcc79bf0f717fd89eee4c
gscarborough <gigimscarborough@gmail.com>
2020-11-30 17:30:20 -0500

    worked on section 3 of queries

--- MovieBuff/movie_buff/03_queries.rb
+++ MovieBuff/movie_buff/03_queries.rb
@@ -1,11 +1,23 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+   length = those_actors.length
+  Movie
+  .select("movies.title, movies.id")
+  .joins(:actors)
+  .where(actors: {name: those_actors})
+  .group("movies.id")
+  .having("COUNT(actors.name) = ?", length)
 
+  # Movie.select("movies.title, movies.id").joins(:actors).where(actors: {name: those_actors})
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
+  Movie
+  .select(:yr)
+  .group("AVG(score)")
+
 
 end
 
@@ -13,11 +25,23 @@ def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
+  selected_movies = Movie.select(:id).joins(:actors).where(actors: {name: name})
 
+  Actor
+  .joins(:movies)
+  .where(movies: {id: selected_movies})
+  .where.not(actors: {name: name})
+  .distinct
+  .pluck(:name)
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
+  Actor
+  .left_outer_joins(:movies)
+  .where("movies.id IS NULL")
+  .pluck("COUNT(actors.id)")
+  .first
 
 end
 
@@ -28,6 +52,12 @@ def starring(whazzername)
 
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
   # not like "stallone sylvester" or "zylvester ztallone"
+  hername = whazzername.split("").join("%")
+
+  Movie
+  .select(:id, :title)
+  .joins(:actors)
+  .where("actors.name ILIKE ?", "%#{hername}%")
 
 end
 
@@ -36,5 +66,12 @@ def longest_career
   # (the greatest time between first and last movie).
   # Order by actor names. Show each actor's id, name, and the length of
   # their career.
+  Actor
+  .select(:id, :name, "MAX(movies.yr) - MIN(movies.yr) as career")
+  .joins(:movies)
+  .group(:id)
+  .order("career DESC")
+  .limit(3)
+
 
 end
